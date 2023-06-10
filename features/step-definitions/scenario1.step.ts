fb5c2fa1f41f5e08de24974a108ebe4197855a37
Tomás <tomasarreguib@gmail.com>
2023-06-10 17:40:41 -0300

    test (bdd): basic implementation for then scenario
    test (bdd): added basic implementation for when scenario
    
    test (bdd): basic implementation to test given scenario
    
    test (bdd): import assert for test scenario

--- features/step-definitions/scenario1.step.ts
+++ features/step-definitions/scenario1.step.ts
@@ -1,13 +1,23 @@
+import { strict as assert } from 'assert';
 import { Given, When, Then } from '@cucumber/cucumber';
 
-Given('que quiero aprender sobre {string}', function (string: any) {
- return string
+type Book = { name: string }
+
+class BookService {
+  find(): Book[] {
+    return [{ name: "tdd" }]
+  }
+}
+
+Given('que quiero aprender sobre tdd', function () {
+  this.service = new BookService()
 });
 
-When('escribo {string} en el sistema', function (string: any) {
- return string;
+When('escribo {string} en el sistema', function (string: string) {
+  this.result = this.service.find(string);
+  return this.result
 });
 
 Then('el sistema me debe retornar una lista de los mejores libros relacionados', function () {
- return 'pending';
+  assert.deepStrictEqual(this.result, [{ name: "tdd" }])
 });
