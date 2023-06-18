87c758093714b2382a70e9eb1f496b50c85f7d5e
Tomás <tomasarreguib@gmail.com>
2023-06-17 23:26:18 -0300

    test (agent): test for real api agent

--- __tests__/unit-tests/scenario1.spec.ts
+++ __tests__/unit-tests/scenario1.spec.ts
@@ -1,5 +1,6 @@
 import { BookService } from '../../src/services/book.service';
 import { BookFactory } from '../factory/BookFactory';
+import { BookServiceApiAgent } from '../../src/agents/BookServiceApiAgent';
 
 test('book service should be defined', () => {
   const bookService = new BookFactory().build()
@@ -15,4 +16,10 @@ test('should find a book', () => {
 test('BookFactory should receive an agent as parameter or implement a fake agent by default', () => {
   const bookService = new BookFactory().build();
   expect(bookService).toBeInstanceOf(BookService);
+})
+
+test('BookService should return a a list of books', () => {
+  const bookService = new BookFactory(new BookServiceApiAgent()).build();
+  const books = bookService.find('tdd');
+  expect(books.length).toBeGreaterThan(0)
 })
