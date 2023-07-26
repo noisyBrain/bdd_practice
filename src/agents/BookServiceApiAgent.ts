a94bd9445f304e03cd740361fcc6d50736a5591f
Tomás <tomasarreguib@gmail.com>
2023-07-26 19:56:54 -0300

    fix(prettier): applied formatter

--- src/agents/BookServiceApiAgent.ts
+++ src/agents/BookServiceApiAgent.ts
@@ -1,4 +1,7 @@
-import { BookApiInterface } from "../interfaces/BookApi.interface";
+import axios from 'axios';
+
+import { BookApiInterface } from '../interfaces/BookApi.interface';
+import { Book } from './types/Book';
 
 export class BookServiceApiAgent implements BookApiInterface {
   findBook(_name: string): Object[] {
